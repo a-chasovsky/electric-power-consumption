fd022dada0d359a8217068822a563639d4f48aa5
Александр Часовский <96054888+a-chasovsky@users.noreply.github.com>
2024-03-16 14:11:09 +0300

    update

--- docs/index-script.js
+++ docs/index-script.js
@@ -3,7 +3,7 @@ let mybutton = document.getElementById('toTocBtn');
     // When the user scrolls down 300px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
