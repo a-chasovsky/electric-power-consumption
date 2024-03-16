93af88ff2a756536b689ff62d7a45205c73f2619
Александр Часовский <96054888+a-chasovsky@users.noreply.github.com>
2024-03-16 14:41:21 +0300

    update

--- docs/index-script.js
+++ docs/index-script.js
@@ -1,5 +1,5 @@
 // Button 'Наверх'
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById('toTocBtn');
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
